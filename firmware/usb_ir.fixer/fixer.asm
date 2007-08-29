69b0af306b2f5e4c61ee46b79a5882b8faab22b6
Joe <jdunn14@users.noreply.github.com>
2007-08-29 17:14:37 +0000

    Fixer now makes the version 4 devices not reset after each page write.

--- firmware/usb_ir.fixer/fixer.asm
+++ firmware/usb_ir.fixer/fixer.asm
@@ -1,3 +1,5 @@
+include "m8c.inc"
+
 export fixer_start
 
 ; addresses for SROM param block taken from the data sheet
@@ -9,54 +11,56 @@ CLOCK:				EQU 0xFC
 DELAY:				EQU 0xFE
 
 AREA bss
-    BLK 100
 buffer:
     BLK 64
 tmp1:
     BLK 1
-tmp3:
+tmp2:
     BLK 1
+stack:
 
-AREA text
+AREA text;_pinned (ROM, ABS, CON)
+;org 0x1fc0
 fixer_start:
-    and F, ~0x01
+    ; something was messed up in the stack, move it
+	mov A, stack
+	swap A, SP
 
-	mov [tmp3], 51
+	; lock
+	and F, 0xFE
 
-	; do a block read
-	mov A, 0x01 ;read block 
+	; set the page once
+	mov [tmp2], 0x33
+
+	; read
+	mov [tmp1], 0x01
 	call exec_ssc
 
-	; change that one byte
+	; modify
 	mov [buffer + 7], 0x09
 
-	; do a block erase
-	mov A, 0x03 ;erase block code
+	; erase
+	mov [tmp1], 0x03
 	call exec_ssc
 
-	;now set up the parameter block for the SROM call
-	mov A, 0x02 ;write block code
+	; write
+	mov [tmp1], 0x02
 	call exec_ssc
 
-    or F, 0x01
-    ljmp 0 ; hard reset the device
+	; unlock
+	or F, 0x01
 
-; pre: tmp3 holds the block index
-;      A holds the ssc code
 exec_ssc:
-	; store the ssc code
-	mov [tmp1], A
-
 	;now set up the parameter block for the SROM call
+	mov [BLOCKID], [tmp2]       ; set which flash block to write
+	mov [POINTER], buffer       ; write data to the buffer
+	mov [CLOCK], 0x00           ; guessing at the right clock divider
+	mov [DELAY], 0xAC           ; this is a guess, since datasheet says use 0x56 for 12MHz
 	mov [KEY1], 0x3A;
 	mov X, SP                   ; get stack pointer
 	mov A, X
 	add A, 3                    ; just following directions from datasheet
 	mov [KEY2], A
-	mov [BLOCKID], [tmp3] ; set which flash block to write
-	mov [POINTER], buffer       ; write data to the buffer
-	mov [CLOCK], 0x00           ; guessing at the right clock divider
-	mov [DELAY], 0xAC           ; this is a guess, since datasheet says use 0x56 for 12MHz
 	mov A, [tmp1]               ; load the ssc code
 	ssc                         ; execute it
 	ret
