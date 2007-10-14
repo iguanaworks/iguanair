9ee1e26c5222ef14ad8ed7c5c8b1b5d20e68b4b9
Ben <IguanaBen@users.noreply.github.com>
2007-10-14 17:58:18 +0000

    more liscense stuff

--- software/usb_ir/guiclient/IguanaClient.py
+++ software/usb_ir/guiclient/IguanaClient.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python
 
+#***************************************************************************
+#** IguanaClient.py ********************************************************
+# **************************************************************************
+# *
+# * TODO: DESCRIBE AND DOCUMENT THIS FILE
+# *
+# * Copyright (C) 2007, IguanaWorks Incorporated (http://iguanaworks.net)
+# * Author: Joseph Dunn <jdunn@iguanaworks.net>
+# *
+# * Distributed under the GPL version 2.
+# * See LICENSE for license details.
+# */
+
+
 import os
 import sys
 
--- software/usb_ir/win32/pipes.c
+++ software/usb_ir/win32/pipes.c
@@ -1,3 +1,16 @@
+/**************************************************************************
+ * pipes.c **************************************************************
+ **************************************************************************
+ *
+ * TODO: DESCRIBE AND DOCUMENT THIS FILE
+ *
+ * Copyright (C) 2007, IguanaWorks Incorporated (http://iguanaworks.net)
+ * Author: Joseph Dunn <jdunn@iguanaworks.net>
+ *
+ * Distributed under the GPL version 2.
+ * See LICENSE for license details.
+ */
+
 #include <stdio.h>
 
 #include "base.h"
