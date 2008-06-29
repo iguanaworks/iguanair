76bb8e7ba54f5eca9b666d6c0126b32a9403be13
Joe <jdunn14@users.noreply.github.com>
2008-06-29 02:05:40 +0000

    New version of the body code forcing a default carrier frequency and adding a cycle count to the --get-features response.

---
+++ body.asm
@@ -0,0 +1,244 @@
+;**************************************************************************
+; * body.asm ***************************************************************
+; **************************************************************************
+; *
+; * This implements the basic firmware body code.
+; *
+; * Copyright (C) 2007, IguanaWorks Incorporated (http://iguanaworks.net)
+; * Author: Joseph Dunn <jdunn@iguanaworks.net>
+; *
+; * Distributed under the GPL version 2.
+; * See LICENSE for license details.
+; */
+
+include "m8c.inc"    ; part specific constants and macros
+include "loader.inc"
+include "body.inc"
+
+VERSION_ID_LOW:  equ 0x04 ; firmware version ID low byte (code body)
+
+export get_feature_list
+
+; FUNCTION: body_main
+;  API used by the loader to call into the body.
+;  pre: A contains a control code
+;       ctl_packet contains the whole control packet
+body_main:
+    ; save the control code
+    mov [tmp1], A
+
+    ; check if we need to initialize or reset
+    mov A, [loader_flags]
+    and A, FLAG_BODY_INIT
+    jz bm_was_initialized
+
+    ;  INITIALIZE
+    ; enable output for tx on all channels
+    call get_feature_list
+    and A, HAS_LEDS | HAS_BOTH | HAS_SOCKETS
+    jnz bm_output_channels
+    mov REG[P06CR], 0x01 ; only had 1 channel back then
+    jmp bm_output_enabled
+  bm_output_channels:
+    mov REG[P14CR], 0x01 ; channel 0
+    mov REG[P15CR], 0x01 ; channel 1
+    mov REG[P16CR], 0x01 ; channel 2
+    mov REG[P17CR], 0x01 ; channel 3
+  bm_output_enabled:
+    ; pins will be set correctly by the rx_reset below
+
+    ; configure capture
+    mov REG[RX_PIN_CR], 0b00000010 ; configure port pin: pullup enabled
+    mov REG[TMRCLKCR],  0b11001111 ; timer to 3MHz
+    mov REG[TMRCR],     0b00001000 ; 16 bit mode
+    mov REG[TCAPINTE],  0b00000011 ; configure rise and fall interrupts
+    ; but don't actually enable the interrupts yet
+
+    ; clear the initialize flag
+    mov A, [loader_flags]
+    and A, ~FLAG_BODY_INIT
+    mov [loader_flags], A
+
+  bm_was_initialized:
+    mov A, [loader_flags]
+    and A, FLAG_BODY_RESET
+    jz bm_was_reset
+
+    ; SOFT RESET
+    mov [rx_on], 0   ; rx starts in the off state
+    lcall rx_reset   ; make sure receiver state matches
+    lcall pins_reset ; clear GPIO pin state
+
+    ; clear the soft reset flag
+    mov A, [loader_flags]
+    and A, ~FLAG_BODY_RESET
+    mov [loader_flags], A
+
+  bm_was_reset:
+    ; reload the control code
+    mov A, [tmp1]
+
+    cmp A, CTL_GETFEATURES
+    jz get_features_body
+
+    cmp A, CTL_GETBUFSIZE
+    jz get_buf_size_body
+
+    ; receive functions
+    cmp A, CTL_RECVON
+    jz recv_on_body
+    cmp A, CTL_RECVOFF
+    jz recv_off_body
+
+    ; send functions
+    cmp A, CTL_SEND
+    jz send_body
+
+    ; pin functions
+    cmp A, CTL_GETPINCONFIG
+    jz get_pin_config_body
+    cmp A, CTL_SETPINCONFIG
+    jz set_pin_config_body
+    cmp A, CTL_GETPINS
+    jz get_pins_body
+    cmp A, CTL_SETPINS
+    jz set_pins_body
+    cmp A, CTL_PINBURST
+    jz pin_burst_body
+
+    ; misc functions
+    cmp A, CTL_EXECUTE
+    jz execute_body
+
+    ; that's everything we handle
+    jmp bm_ret
+
+  bm_ack_then_ret:
+    ; send ack
+    mov X, CTL_BASE_SIZE
+    lcall write_control
+  bm_ret:
+    ret                ; return to main recv
+
+body_loop_body:
+    ; check for receive overflow
+    mov A, [rx_overflow]
+    jz no_overflow
+    ; send back an overflow packet
+    mov [control_pkt + CCODE], CTL_OVERRECV
+    mov X, CTL_BASE_SIZE
+    lcall write_control
+    lcall rx_reset
+
+  no_overflow:
+    lcall write_signal ; write back received data
+    ret                ; return to main loop
+
+get_features_body:
+    mov [control_pkt + CCODE], CTL_GETFEATURES
+    call get_feature_list
+    mov [control_pkt + CDATA + 0], A
+    ; the numbers (cycle counts) are taken from the ir.asm code
+    mov [control_pkt + CDATA + 1],5 + 5 + 7 + 6 + 6 + 7 + (5 + 7) + (5 + 7) + 5
+    mov X, CTL_BASE_SIZE + 2
+    lcall write_control
+    jmp bm_ret
+
+get_buf_size_body:
+    mov [control_pkt + CCODE], CTL_GETBUFSIZE
+    mov [control_pkt + CDATA], BUFFER_SIZE
+    mov X, CTL_BASE_SIZE + 1
+    lcall write_control
+    jmp bm_ret
+
+recv_on_body:
+    mov [rx_on], 0x1 ; note that rx should be on
+    lcall rx_reset   ; make rx state actually match rx_on
+    jmp bm_ack_then_ret
+
+recv_off_body:
+    mov [rx_on], 0x0 ; note that rx should be off
+    lcall rx_reset   ; make rx state actually match rx_on
+    jmp bm_ack_then_ret
+
+send_body:
+    lcall rx_disable      ; disable timer interrupt, clear rx state
+    lcall read_buffer     ; receive the code--returns 0 if read overflow
+    jz send_body_overflow ; error on overflow
+    lcall transmit_code   ; transmit
+    lcall rx_reset        ; turn rx on if necessary
+    jmp bm_ack_then_ret
+
+  send_body_overflow:
+    lcall transmit_code ; transmit anyway
+    lcall rx_reset      ; turn rx on if necessary
+
+    ; send overflow instead of ack
+    mov [control_pkt + CCODE], CTL_OVERSEND
+
+    ; send ack
+    mov X, CTL_BASE_SIZE + 1
+    lcall write_control
+    jmp bm_ret
+
+get_pin_config_body:
+    lcall get_pin_config
+    jmp bm_ret
+
+set_pin_config_body:
+    lcall set_pin_config
+    jmp bm_ret
+
+; put the pins in the control packet and send it back
+get_pins_body:
+    lcall get_pins
+    mov X, CTL_BASE_SIZE + 2
+    lcall write_control
+    jmp bm_ret
+
+; use control packet pin settings then ack
+set_pins_body:
+    lcall set_pins
+    jmp bm_ack_then_ret
+
+; send a burst of activity to the GPIO pins
+pin_burst_body:
+    ; disable rx since the burst timing would interfer
+    lcall rx_disable
+
+    lcall read_buffer ; get the block to read
+    lcall pin_burst   ; set pins in a burst
+
+    jmp bm_ack_then_ret
+
+; default to executing the final page since nothing else is easy
+execute_body:
+    lcall 0x1FC0
+    jmp bm_ret
+
+
+
+
+; implementation of the body jump table located at BODY_JUMPS
+; Do not modify this code unless you KNOW what you are doing!
+area bodyentry (ROM, ABS, CON)
+org body_version
+    mov A, VERSION_ID_LOW
+    ret
+
+org body_handler
+    jmp body_main
+
+org body_loop
+    jmp body_loop_body
+
+org body_tcap_int
+    jmp tcap_int
+
+org body_twrap_int
+    jmp twrap_int
+
+org BODY_JUMPS + 60
+  get_feature_list:
+    mov A, 0
+    ret
---
+++ pins.asm
@@ -0,0 +1,268 @@
+;**************************************************************************
+; * pins.asm ***************************************************************
+; **************************************************************************
+; *
+; * All the functions for controlling GPIO pins on unenclosed devices.
+; *
+; * Copyright (C) 2007, IguanaWorks Incorporated (http://iguanaworks.net)
+; * Author: Joseph Dunn <jdunn@iguanaworks.net>
+; *
+; * Distributed under the GPL version 2.
+; * See LICENSE for license details.
+; */
+
+include "m8c.inc"    ; part specific constants and macros
+include "loader.inc"
+include "body.inc"
+
+export pins_reset
+export get_pin_config
+export set_pin_config
+export get_pins
+export set_pins
+export pin_burst
+
+has_gpios:
+    call get_feature_list
+    and A, HAS_LEDS | HAS_BOTH | HAS_SOCKETS
+    jnz has_not
+    mov A, 1
+    jmp has_done
+  has_not:
+    mov [control_pkt + CCODE], CTL_INVALID_ARG
+    mov X, CTL_BASE_SIZE
+    lcall write_control
+    mov A, 0
+  has_done:
+    ret
+
+pins_reset:
+    call has_gpios
+    jz reset_done
+
+    ; clear the data registers
+    and REG[P0DATA], 0xF0
+    and REG[P1DATA], 0x87
+
+    ; clear the config registers for pins we use
+    ; port 0, pins 0-3
+    mov REG[P00CR], 0
+    mov REG[P01CR], 0
+    mov REG[P02CR], 0
+    mov REG[P03CR], 0
+
+    ; port 1, pins 3-6
+    mov REG[P13CR], 0
+    mov REG[P14CR], 0
+    mov REG[P15CR], 0
+    mov REG[P16CR], 0
+  reset_done:
+    ret
+
+get_pin_config:
+    call has_gpios
+    jz get_pin_config_done
+
+    ; ack the request
+    mov X, CTL_BASE_SIZE
+    lcall write_control
+
+    mov A, REG[P00CR]   ; read the pin control register
+    and A, PIN_CFG_MASK ; mask off the bits we will accept
+    mov [control_pkt + 0], A ; store pin config byte
+
+    mov A, REG[P01CR]   ; read the pin control register
+    and A, PIN_CFG_MASK ; mask off the bits we will accept
+    mov [control_pkt + 1], A ; store pin config byte
+
+    mov A, REG[P02CR]   ; read the pin control register
+    and A, PIN_CFG_MASK ; mask off the bits we will accept
+    mov [control_pkt + 2], A ; store pin config byte
+
+    mov A, REG[P03CR]   ; read the pin control register
+    and A, PIN_CFG_MASK ; mask off the bits we will accept
+    mov [control_pkt + 3], A ; store pin config byte
+
+    mov A, REG[P13CR]   ; read the pin control register
+    and A, PIN_CFG_MASK ; mask off the bits we will accept
+    mov [control_pkt + 4], A ; store pin config byte
+
+    mov A, REG[P14CR]   ; read the pin control register
+    and A, PIN_CFG_MASK ; mask off the bits we will accept
+    mov [control_pkt + 5], A ; store pin config byte
+
+    mov A, REG[P15CR]   ; read the pin control register
+    and A, PIN_CFG_MASK ; mask off the bits we will accept
+    mov [control_pkt + 6], A ; store pin config byte
+
+    mov A, REG[P16CR]   ; read the pin control register
+    and A, PIN_CFG_MASK ; mask off the bits we will accept
+    mov [control_pkt + 7], A ; store pin config byte
+
+    ; return the config data in a second packet
+    mov X, PACKET_SIZE
+    mov A, control_pkt
+    lcall write_packet
+
+  get_pin_config_done:
+    ret
+
+set_pin_config:
+    call has_gpios
+    jz set_pin_config_done
+
+  wait_data_ready:
+    lcall check_read ; see if there is a transmission from the host
+    jz wait_data_ready
+
+    ; read the new configuration
+    mov X, PACKET_SIZE
+    mov A, control_pkt
+    lcall read_packet
+
+    mov X, PACKET_SIZE
+    mov A, control_pkt
+    lcall write_packet
+
+    mov A, [control_pkt + 0] ; get pin config byte
+    and A, PIN_CFG_MASK ; mask off the bits we will accept
+    mov REG[P00CR], A   ; set the pin control register
+
+    mov A, [control_pkt + 1] ; get pin config byte
+    and A, PIN_CFG_MASK ; mask off the bits we will accept
+    mov REG[P01CR], A   ; set the pin control register
+
+    mov A, [control_pkt + 2] ; get pin config byte
+    and A, PIN_CFG_MASK ; mask off the bits we will accept
+    mov REG[P02CR], A   ; set the pin control register
+
+    mov A, [control_pkt + 3] ; get pin config byte
+    and A, PIN_CFG_MASK ; mask off the bits we will accept
+    mov REG[P03CR], A   ; set the pin control register
+
+    mov A, [control_pkt + 4] ; get pin config byte
+    and A, PIN_CFG_MASK ; mask off the bits we will accept
+    mov REG[P13CR], A   ; set the pin control register
+
+    mov A, [control_pkt + 5] ; get pin config byte
+    and A, PIN_CFG_MASK ; mask off the bits we will accept
+    mov REG[P14CR], A   ; set the pin control register
+
+    mov A, [control_pkt + 6] ; get pin config byte
+    and A, PIN_CFG_MASK ; mask off the bits we will accept
+    mov REG[P15CR], A   ; set the pin control register
+
+    mov A, [control_pkt + 7] ; get pin config byte
+    and A, PIN_CFG_MASK ; mask off the bits we will accept
+    mov REG[P16CR], A   ; set the pin control register
+
+    ; ack the request
+    mov [control_pkt + CCODE], CTL_SETPINCONFIG
+    mov X, CTL_BASE_SIZE
+    lcall write_control
+
+  set_pin_config_done:
+    ret
+
+get_pins:
+    call has_gpios
+    jz get_pins_done
+
+    ; disable ints to avoid race conditions on the ports
+    and F, 0xFE
+    mov A, REG[P0DATA]               ; get the current pin state
+    mov [control_pkt + CDATA + 0], A ; write out the state
+    mov A, REG[P1DATA]               ; get the current pin state
+    mov [control_pkt + CDATA + 1], A ; write out the state
+    or  F, 0x1 ; re-enable global interrupts
+
+    ; no need to move first byte's 4 bits, the other >> 3
+    asr [control_pkt + CDATA + 1]
+    asr [control_pkt + CDATA + 1]
+    asr [control_pkt + CDATA + 1]
+
+    ; mask away the upper bits of both bytes
+    and [control_pkt + CDATA + 0], 0x0F
+    and [control_pkt + CDATA + 1], 0x0F
+
+  get_pins_done:
+    ret
+
+; FUNCTION set_pins
+;   arg: low nibble of first byte has value of port 0 pins
+;        low nibble of second byte has value of port 1 pins
+set_pins:
+    call has_gpios
+    jz set_pins_done
+
+    ; mask away the upper bits of both bytes
+    and [control_pkt + CDATA + 0], 0x0F
+    and [control_pkt + CDATA + 1], 0x0F
+
+    ; no need to move first byte's 4 bits, the other << 3
+    asl [control_pkt + CDATA + 1]
+    asl [control_pkt + CDATA + 1]
+    asl [control_pkt + CDATA + 1]
+
+    ; disable ints to avoid race conditions on the ports
+    and F, 0xFE
+    mov A, REG[P0DATA]              ; get the current pin state
+    and A, 0xF0                     ; mask off the ones we're setting
+    or A, [control_pkt + CDATA + 0] ; set the appropriate bits
+    mov REG[P0DATA], A              ; write out the new value
+
+    mov A, REG[P1DATA]              ; get the current pin state
+    and A, 0x87                     ; mask off the ones we're setting
+    or A, [control_pkt + CDATA + 1] ; set the appropriate bits
+    mov REG[P1DATA], A              ; write out the new value
+
+    or  F, 0x1 ; re-enable global interrupts
+  set_pins_done:
+    ret
+
+; FUNCTION pins_burst
+;   sets the pins to a sequence of values
+;   arg: control packet data byte 0 contains number of transfers
+;   arg: buffer contains sequence info (pin states to set)
+pin_burst:
+    call has_gpios
+    jz reset_done
+
+    mov [buffer_ptr], buffer ; reset to start of buffer
+    ; first get the number of transfers, put in X
+    mov X, [control_pkt + CDATA]
+
+  pin_burst_loop:
+    jz pin_burst_done   ; see if we're done
+    mvi A, [buffer_ptr] ; get the next byte to set
+    mov [tmp1], A       ; keep a copy
+
+    ; set it up in the control packet locations--low nibble in first byte, high in second byte
+    and A, 0x0F
+    mov [control_pkt + CDATA + 0], A
+
+    mov A, [tmp1]
+    and A, 0xF0
+    ; shift right 4
+    asr A
+    asr A
+    asr A
+    asr A
+    mov [control_pkt + CDATA + 1], A
+
+    ; ok, set pins
+    call set_pins
+
+    ; delay a bit
+    mov A, BURST_DELAY
+  pin_burst_delay:
+    jz pin_burst_delend
+    dec A
+    jmp pin_burst_delay
+
+  pin_burst_delend:
+    dec X
+    jmp pin_burst_loop
+
+  pin_burst_done:
+    ret
