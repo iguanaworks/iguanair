9ce16996ee37df6ced3f03880fae98ae060e42fd
Ben <IguanaBen@users.noreply.github.com>
2008-07-26 16:04:56 +0000

    Added script to get lirc from source and compile it with a localized IguanaWorks name

---
+++ software/usb_ir/packaging/debian/PackageLirc.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+export DEBEMAIL="support@iguanaworks.net"
+export DEBFULLNAME="IguanaWorks"
+LIRCTMP=/tmp/lirc-temp
+mkdir $LIRCTMP
+pushd $LIRCTMP
+apt-get source lirc
+cd lirc-*
+dch -l IguanaWorks "Compiled with support for IguanaWorks USB IR Transceiver (driver: iguanaIR)"
+fakeroot debian/rules binary
+popd
+cp $LIRCTMP/lirc_*.deb .
+rm -rf $LIRCTMP
