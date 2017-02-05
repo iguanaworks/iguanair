283e98848e18215a762a8c7dc4d48b2f63907dfc
Alec Leamas <leamas.alec@gmail.com>
2017-02-05 13:27:30 +0100

    build: Adjust installation paths to proper Debian/FHS/systemd standards.
    Current paths has only been tested on Fedora where /usr/lib and
    /lib are the same locations due to the UsrMove. Adjust paths to
    properly use /lib instead of /usr/lib as required. This opens a can of
    worms, since systemd does *not* define the installation paths in a
    fixed manner - they need to be configured using pkg-config.
    
    Also fixes issue with make-dist.sh not creating the same paths as
    the git patches. Allow user-defined tarball name.
    
    Packages has been built on both Fedor 25a and Debian Stretch from
    these sources with minimal downstream patching.

--- make-dist.sh
+++ make-dist.sh
@@ -2,28 +2,28 @@
 #
 # What: create source distribution, requires a git tree.
 #
-# Usage: make_dist [tree-ish]
+# Usage: [TARNAME=...] make_dist [tree-ish]
 #
 # runCmake should have been run, since it creates version.h which
 # cannot be re-created outside the git tree. The script build also uses
 # release.h created by ./runCmake
 #
 # Contrary to the cmake code, this script only includes sources as
-# defined by git (+ version.h, version.h) which makes it less error
+# defined by git (+ version.h, release.h) which makes it less error
 # prone.
 
 set -e
 cd $(dirname $(readlink -f $0))
 
 treeish=${1:-'HEAD'}
-release=$(
-    sed -rne '/IGUANAIR_RELEASE/s/.*"([^"]+)".*/\1/p' < usb_ir/release.h )
-tarname="IguanaIR-$release"
+release=$( sed -rne '/IGUANAIR_RELEASE/s/.*"([^"]+)".*/\1/p' \
+    < software/usb_ir/release.h )
+tarname=${TARNAME:-"IguanaIR-$release"}
 
 test -d dist || mkdir dist
 git archive --prefix $tarname/ --output dist/$tarname.tar $treeish
 tar --append --transform "s|^\.|$tarname|"  -f dist/$tarname.tar \
-    ./usb_ir/version.h ./usb_ir/release.h
+    ./software/usb_ir/version.h ./software/usb_ir/release.h
 gzip -f dist/$tarname.tar
 sha256sum dist/$tarname.tar.gz > dist/$tarname.sum
 
