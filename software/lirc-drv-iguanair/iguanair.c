8d5c4af9ce76685ff946a36c2433ec6e19d3a31a
Joseph Dunn <jdunn@iguanaworks.net>
2017-07-24 14:45:13 -0400

    Snapshot of the lirc driver mostly working.  There are a couple TODOs, but I wanted to make sure to checkpoint the basics.

--- software/lirc-drv-iguanair/iguanair.c
+++ software/lirc-drv-iguanair/iguanair.c
@@ -11,6 +11,7 @@
 */
 
 #include "lirc_driver.h"
+#include "lirc/config.h"
 #include "iguanaIR.h"
 
 #include <errno.h>
@@ -22,6 +23,11 @@
   #include <wait.h>
 #endif
 
+/* Uncomment the following line to change how we describe devices when
+   enumerating
+#define DESCRIBE_DEVICES
+*/
+
 static const logchannel_t logchannel = LOG_DRIVER;
 static int sendConn = -1;
 static pid_t child = 0;
@@ -31,6 +37,13 @@ static int currentCarrier = -1;
 static void quitHandler(int sig)
 {
     recvDone = 1;
+}
+
+static int connectToIgdaemon(const char *device)
+{
+    if (strcmp(device, "auto") == 0)
+        device = NULL;
+    return iguanaConnect(device);
 }
 
 static void recv_loop(int fd, int notify)
@@ -47,7 +60,7 @@ static void recv_loop(int fd, int notify)
     /* notify parent by closing notify */
     close(notify);
 
-    conn = iguanaConnect(drv.device);
+    conn = connectToIgdaemon(drv.device);
     if (conn != -1)
     {
         iguanaPacket request, response;
@@ -140,6 +153,24 @@ static void recv_loop(int fd, int notify)
     close(fd);
 }
 
+static pid_t dowaitpid(pid_t pid, int* stat_loc, int options)
+{
+    pid_t retval;
+
+    do
+        retval = waitpid(pid, stat_loc, options);
+    while (retval == (pid_t)-1 && errno == EINTR);
+
+    return retval;
+}
+
+static void killChildProcess()
+{
+    /* signal the child process to exit */
+    if (child > 0 && (kill(child, SIGTERM) == -1 || dowaitpid(child, NULL, 0) != (pid_t)-1))
+        child = 0;
+}
+
 static int iguana_init()
 {
     int recv_pipe[2], retval = 0;
@@ -185,28 +216,18 @@ static int iguana_init()
                 /* make sure child has set its signal handler to avoid race with iguana_deinit() */
                 chk_read(notify[0], &dummy, 1);
                 close(notify[0]);
-                sendConn = iguanaConnect(drv.device);
+                sendConn = connectToIgdaemon(drv.device);
                 if (sendConn == -1)
                 {
                     log_error("couldn't open connection to iguanaIR daemon: %s",
                               strerror(errno));
+                    killChildProcess();
                 }
                 else
                     retval = 1;
             }
         }
     }
-
-    return retval;
-}
-
-static pid_t dowaitpid(pid_t pid, int* stat_loc, int options)
-{
-    pid_t retval;
-
-    do
-        retval = waitpid(pid, stat_loc, options);
-    while (retval == (pid_t)-1 && errno == EINTR);
 
     return retval;
 }
@@ -220,9 +241,8 @@ static int iguana_deinit()
         sendConn = -1;
     }
 
-    /* signal the child process to exit */
-    if (child > 0 && (kill(child, SIGTERM) == -1 || dowaitpid(child, NULL, 0) != (pid_t)-1))
-        child = 0;
+    /* shut down the child reader */
+    killChildProcess();
 
     /* close drv.fd since otherwise we leak open files */
     close(drv.fd);
@@ -328,16 +348,15 @@ static int iguana_send(struct ir_remote* remote, struct ir_ncode* code)
 
 #if VERSION_NODOTS >= 1000
 
-static int list_devices(glob_t* glob)
+static int list_devices(glob_t* devices)
 {
-    const char *devList, *pos;
-    glob_t devices;
+    char *devList, *pos;
     glob_t_init(devices);
 
     pos = devList = iguanaListDevices();
     if (devList != NULL)
     {
-        char *start, idx[8], type;
+        char *start, *idx, type;
         while(pos[0] != '\0')
         {
             type = *pos;
@@ -355,27 +374,51 @@ static int list_devices(glob_t* glob)
 
             if (pos != start)
             {
-                char s = (char*)malloc(60);
-                strncpy(s, start, pos - start);
-                s[pos - start] = '\0';
+                char buf[13] = { '\0' }, *s;
+
+                strncpy(buf, start, pos - start);
+#ifdef DESCRIBE_DEVICES
+                s = (char*)malloc(strlen(IGSOCK_NAME) + strlen(buf) + 42 + 8);
+#else
+                s = (char*)malloc(strlen(IGSOCK_NAME) + strlen(buf) + 42 + strlen(IGSOCK_NAME) + 8);
+#endif
+                sprintf(s, "%s%s", IGSOCK_NAME, buf);
+
                 if (type == 'i')
-                    strcpy(idx, s);
+#ifdef DESCRIBE_DEVICES
+                    idx = strdup(buf);
+#else
+                    idx = strdup(s);
+#endif
 
                 /* First word is the device path, the rest optional
                    free-format info on device usable in a UI. */
                 switch(type)
                 {
                 case 'i':
-                    strcat(s, " IguanaWorks USB Infrared device");
+#ifdef DESCRIBE_DEVICES
+                    strcat(s, " IguanaWorks USB Infrared device ");
+                    strcat(s, idx);
+#else
+                    strcat(s, " [1781:0938]");
+#endif
                     break;
 
                 case 'l':
+#ifdef DESCRIBE_DEVICES
                     strcat(s, " Hardware location based alias for device ");
+#else
+                    strcat(s, " ->");
+#endif
                     strcat(s, idx);
                     break;
 
                 case 'u':
+#ifdef DESCRIBE_DEVICES
                     strcat(s, " User specified alias for device ");
+#else
+                    strcat(s, " ->");
+#endif
                     strcat(s, idx);
                     break;
                 }
@@ -388,7 +431,7 @@ static int list_devices(glob_t* glob)
         free(devList);
     }
 
-    return devices;
+    return 0;
 }
 
 #endif
@@ -396,32 +439,36 @@ static int list_devices(glob_t* glob)
 
 /*
  * Set the transmit channel(s) bitmask:.
- * Return 0 on success, 4  out-of-range, -1 on other errors. See
+ * Return 0 on success, 4 out-of-range, -1 on other errors. See
  * LIRC_SET_TRANSMITTER in lirc(4)
  */
+// TODO: what is the correct out-of-range return
 static int set_transmitters(uint8_t channels)
 {
-       if (channels > 0x0F)
-               return DRV_ERR_BAD_VALUE;
-       if (daemonTransaction(IG_DEV_SETCHANNELS, &channels, sizeof(channels)))
-               return 0;
-       return -1;
-       // FIXME: Should return DRV_ERR_BAD_VALUE, DRV_ERR_INTERNAL, DRV_ERR_BAD_STATE?
+    if (channels > 0x0F)
+        return DRV_ERR_BAD_VALUE;
+    else if (! daemonTransaction(IG_DEV_SETCHANNELS, &channels, sizeof(channels)))
+        return DRV_ERR_INTERNAL;
+    return 0;
 }
 
 
 static int drvctl_func(unsigned int cmd, void* arg)
 {
-    switch (cmd) {
+    switch (cmd)
+    {
 #if VERSION_NODOTS >= 1000
     case DRVCTL_GET_DEVICES:
         return list_devices((glob_t*) arg);
+
     case DRVCTL_FREE_DEVICES:
         drv_enum_free((glob_t*) arg);
         return 0;
 #endif
+
     case LIRC_SET_TRANSMITTER_MASK:
         return set_transmitters(*(uint8_t*) arg);
+
     default:
         return DRV_ERR_NOT_IMPLEMENTED;
     }
