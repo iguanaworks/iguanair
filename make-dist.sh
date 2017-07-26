c99b49608ffb5df6cb18fdab058501268150299c
Joseph Dunn <jdunn@iguanaworks.net>
2017-07-26 12:14:58 -0400

    significantly more complicated make-dist.sh script working  * you can package any specific commit  * git describe will be used to make the version.h  * switch to /bin/sh (needs testing on non-bash systems)  * will only package committed releases and are always clean

--- make-dist.sh
+++ make-dist.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # What: create source distribution, requires a git tree.
 #
@@ -10,19 +10,54 @@
 # Contrary to the cmake code, this script only includes sources as
 # defined by git (+ version.h) which makes it less error prone.
 
-set -e
-cd $(dirname $(readlink -f $0))
+usb_ir=software/usb_ir
 
+
+# handle errors by just exiting
+set -E
+trap 'exit' ERR
+
+# directory prep
+cd $(dirname $(readlink -f $0))
+test -d dist || mkdir dist
+
+# default to tree "HEAD"
 treeish=${1:-'HEAD'}
-release=$( sed -rne '/Set\(FULLVER/s/[^ ]+ ([^\)]+)\).*/\1/p' \
-               < software/usb_ir/CMakeLists.txt )
+
+# test that treeish is valid before proceeding
+git describe --always --tags ${treeish} > /dev/null
+
+# read the release name from CMakeLists.txt and create tarname
+release=$( git archive ${treeish} software/usb_ir/CMakeLists.txt |
+               tar -x --to-stdout |
+               sed -rne '/Set\(FULLVER/s/[^ ]+ ([^\)]+)\).*/\1/p' )
 tarname=${TARNAME:-"IguanaIR-$release"}
 
-test -d dist || mkdir dist
-git archive --prefix $tarname/ --output dist/$tarname.tar $treeish
-tar --append --transform "s|^\.|$tarname|"  -f dist/$tarname.tar \
-    ./software/usb_ir/version.h
-gzip -f dist/$tarname.tar
-sha256sum dist/$tarname.tar.gz > dist/$tarname.sum
+# create a complete archive of the selected tree
+git archive --prefix ${tarname}/ --output dist/${tarname}.tar ${treeish}
 
-ls -l dist/$tarname.tar.gz
+# complain if the git tag does not match the FULLVER line
+git_version=$( git describe --always --tags ${treeish} )
+git_release=$( git describe --always --abbrev=0 --tags ${treeish} )
+if [ "${release}" != "${git_release}" ]; then
+    echo "CMakeLists.txt FULLVER (${release}) differs from git tag (${git_release}) for ${treeish}"
+
+    git_version="${release}!=${git_version}"
+    git_release="${release}!=${git_release}"
+fi
+
+# generate a version.h similarly to how CMake does it
+tar -xf dist/${tarname}.tar ${tarname}/${usb_ir}/version.h.in --to-stdout > /dev/null
+tar -xf dist/${tarname}.tar ${tarname}/${usb_ir}/version.h.in --to-stdout |
+    sed "s|\${GIT_VER_STR}|${git_version}|" |
+    sed "s|\${GIT_REL_STR}|${git_release}|" > version.h
+echo "Packaging ${git_version}"
+
+# remove version.h.in and add version.h into the tarball, gzip, and sum it
+tar --delete --file dist/${tarname}.tar ${tarname}/${usb_ir}/version.h.in
+tar --remove-files --append --transform "s|^|${tarname}/${usb_ir}/|" \
+    -f dist/${tarname}.tar version.h
+gzip -f dist/${tarname}.tar
+sha256sum dist/${tarname}.tar.gz > dist/${tarname}.sum
+
+ls -l dist
