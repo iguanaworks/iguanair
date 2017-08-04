f6097f4804d92c2a7747e813cba1556d46aa1eab
Joseph Dunn <jdunn@iguanaworks.net>
2017-08-04 11:15:39 -0400

    use a settings file to override the default cmake location/version on windows

---
+++ software/usb_ir/settings.bat
@@ -0,0 +1,4 @@
+REM SET PYPATH=C:\Python27
+REM SET PYPATH="C:\Program Files (x86)\Python36-32"
+REM SET CMAKE="C:\Program Files (x86)\CMake 2.8\bin\cmake.exe"
+SET CMAKE="C:\Program Files\CMake\bin\cmake.exe"
